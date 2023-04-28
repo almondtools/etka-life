148dc00c1a11ec5de0746a9067b91b913a755229
Patrick Neulichedl <patrickneu@hotmail.de>
2023-04-28 17:41:38 +0200

    Implement game of life using dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use crate::universe::Universe;
 use dioxus::prelude::*;
 use game_of_life::{GameOfLife, CELL_SIZE};
+use std::cell::RefCell;
 use std::time::Duration;
 use wasm_bindgen::JsCast;
 
 mod game_of_life;
 mod universe;
+
+const CONTAINER_STYLE: &str = r"
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+";
 
 fn main() {
     dioxus_web::launch(App);
@@ -16,17 +23,44 @@ fn App(cx: Scope) -> Element {
     let universe = Universe::new();
     let canvas_height = (CELL_SIZE + 1) * universe.height() + 1;
     let canvas_width = (CELL_SIZE + 1) * universe.width() + 1;
+    let is_playing = use_state(cx, || true);
 
+    let game_of_life = use_state(cx, || RefCell::new(None::<GameOfLife>));
+
+    let game_of_life_init: UseState<RefCell<Option<GameOfLife>>> = game_of_life.clone();
     use_effect(cx, (), move |()| async move {
         let context = get_context_2d();
-        let mut game_of_life = GameOfLife::new(context, universe);
-        loop {
-            game_of_life.tick();
+        *game_of_life_init.borrow_mut() = Some(GameOfLife::new(context, universe));
+    });
+
+    let game_of_life_play = game_of_life.clone();
+    use_effect(cx, (is_playing,), move |(is_playing,)| async move {
+        while *is_playing.current() {
+            if let Some(game_of_life) = game_of_life_play.borrow_mut().as_mut() {
+                game_of_life.tick();
+            }
             async_std::task::sleep(Duration::from_millis(1)).await;
         }
     });
 
-    cx.render(rsx! {canvas { id: "game-of-life-canvas", height: canvas_height as i64, width: canvas_width as i64 }})
+    cx.render(rsx! {
+        div { style: "{CONTAINER_STYLE}",
+            PlayButton { is_playing: is_playing }
+            canvas { id: "game-of-life-canvas", height: canvas_height as i64, width: canvas_width as i64 }
+        }
+    })
+}
+
+#[inline_props]
+#[allow(non_snake_case)]
+fn PlayButton<'a>(cx: Scope<'a>, is_playing: &'a UseState<bool>) -> Element {
+    cx.render(rsx! {
+        button { onclick: move |_| {
+                is_playing.modify(|value| !value);
+            },
+            if *is_playing.get() { "⏸" } else { "▶" }
+        }
+    })
 }
 
 fn get_context_2d() -> web_sys::CanvasRenderingContext2d {
