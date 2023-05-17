29fa63e84641eda0918b1654b86119460c7bbc87
Stefan Mandel <Stefan.Mandel@andrena.de>
2023-05-17 16:05:51 +0200

    prepare snippets/templates

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const ALIVE_COLOR = "#000000";
 const DEAD = 0;
 const ALIVE = 1;
 
-const universe = Universe.new();
-const width = universe.width();
-const height = universe.height();
+//TODO initUniverse
 
 const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
@@ -31,52 +29,15 @@ const renderLoop = () => {
 };
 
 const drawGrid = () => {
-  ctx.beginPath();
-  ctx.strokeStyle = GRID_COLOR;
-
-
-  for (let i = 0; i <= width; i++) {
-    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-  }
-
-  for (let j = 0; j <= height; j++) {
-    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-  }
-
-  ctx.stroke();
+  //TODO drawGridBody
 };
 
 const drawCells = () => {
-  const cellsPtr = universe.cells();
-  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
-
-  ctx.beginPath();
-
-  drawCellsOfType(cells, ALIVE, ALIVE_COLOR);
-  drawCellsOfType(cells, DEAD, DEAD_COLOR);
-
-  ctx.stroke();
+  //TODO drawCellsBody
 };
 
 const drawCellsOfType = (cells, condition, style) => {
-  ctx.fillStyle = style;
-  for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
-      const idx = getIndex(row, col);
-      if (cells[idx] !== condition) {
-        continue;
-      }
-
-      ctx.fillRect(
-        col * (CELL_SIZE + 1) + 1,
-        row * (CELL_SIZE + 1) + 1,
-        CELL_SIZE,
-        CELL_SIZE
-      );
-    }
-  }
+  //TODO drawCellsOfTypeBody
 };
 
 const getIndex = (row, column) => {
