7938e907fae501578a44f1730fcf8e7edf8438d6
Patrick Neulichedl <patrickneu@hotmail.de>
2023-06-09 08:27:48 +0200

    Remove drawGrid from snippets

--- index.js
+++ index.js
@@ -26,12 +26,26 @@ const renderLoop = () => {
   animationId = requestAnimationFrame(renderLoop);
 };
 
-const drawGrid = () => {
-  // drawGridBody
-};
-
 const drawCells = () => {
   // drawCellsBody
+};
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
 };
 
 const playPauseButton = document.getElementById("play-pause");
